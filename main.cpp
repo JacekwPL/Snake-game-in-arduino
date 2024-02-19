099127def5c81f1f7b870404e649e37d6df445b5
JacekwPL <38696493+JacekwPL@users.noreply.github.com>
2024-02-19 18:00:39 +0100

    SnakeLogic update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
-struct Vector{
+#ifdef _WIN32
+#include <windows.h>
+
+void sleep(unsigned milliseconds)
+{
+    Sleep(milliseconds);
+}
+#else
+#include <unistd.h>
+
+void sleep(unsigned milliseconds)
+{
+    usleep(milliseconds * 1000); // takes microseconds
+}
+#endif
+
+struct Vector {
 public:
     int x, y;
 
@@ -28,10 +45,9 @@ public:
 
 class Movement {
 private:
-
     Vector* pos = new Vector();
     Vector* dir = new Vector();
-
+    Movement* next = nullptr;
 public:
     Vector* getPos() {
         return *&pos;
@@ -41,8 +57,19 @@ public:
         return *&dir;
     }
 
-    Movement() = default;
+    Vector* index(int i) {
+        if (i == 0)
+            return *&pos;
+        if (next)
+            return next->index(i - 1);
+        throw exception("Blad w indeksie MOVEMENT");
+    }
 
+    Movement() {
+        pos->update(0, 0);
+        dir->update(0, 0);
+    }
+    
     Movement(int x, int y) {
         pos->update(x, y);
     }
@@ -52,9 +79,14 @@ public:
         dir->update(xd, yd);
     }
 
-    void move() {
+    void Move() {
+        if (next) {
+            next->updateDir(pos->x - next->pos->x, pos->y - next->pos->y);
+            next->Move();
+        }
         pos->x += dir->x;
         pos->y += dir->y;
+
     }
 
     void updateDir(int x, int y) {
@@ -70,24 +102,123 @@ public:
     ~Movement() {
         delete pos;
         delete dir;
+        delete next;
+    }
+
+    void Add(Movement* ptrMovement) {
+        if (next)
+            next->Add(ptrMovement);
+        else
+            next = ptrMovement;
     }
 
 };
 
+class Snake {
+    Movement* _head;
+    int count = 0;
+public:
+    int Lenght() {
+        return count;
+    }
+
+    Snake(Movement* head) {
+        _head = head;
+        count += 1;
+    }
+    
+    void Move() {
+        if (_head)
+            _head->Move();
+    }
+    void MoveDir(int x, int y) {
+        if (_head)
+            _head->updateDir(x, y);
+            _head->Move();
+    }
+    Vector* Index(int i) {
+        if (_head)
+            return _head->index(i);
+        else
+            throw exception("Blad w indeksie SNAKE");
+    }
+    void Add(Movement* ptrMovement) {
+        if (_head) {
+            _head->Add(ptrMovement);
+        }
+        else
+            _head = ptrMovement;
+        count++;
+    }
+
+    ~Snake() {
+        delete _head;
+    }
+};
+
+bool IsSnakePos(int x, int y, Snake* ptrSnake) {
+    Vector* arr;
+    for (unsigned char i = 0; i < ptrSnake->Lenght(); i++)
+    {
+        arr = ptrSnake->Index(i);
+        if (arr->x == x && arr->y == y) { return true; }
+    }
+    return false;
+}
+
+void DrawScreen(Snake* ptr) {
+    cout << "|||||||||||||||||||||||||||";
+    for (unsigned char i = 0; i < 10; i++)
+    {
+        for (unsigned char j = 0; j < 10; j++)
+        {
+            if (IsSnakePos(j, i, ptr)) {
+                cout << "#";
+            }
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
 
 
 
 int main() {
-    auto start = new Movement(5, 5, 1, 0);
-    auto start2 = new Movement(5, 5);
+    Movement* ptrStart0 = new Movement(5, 5, 1, 0);
+    Movement* ptrStart1 = new Movement(5, 4);
+    Movement* ptrStart2 = new Movement(5, 3);
+    Snake* ptrSnake = new Snake(ptrStart0);
 
-    start->print();
-    start->move();
-    start->print();
+    ptrSnake->Add(ptrStart1);
+    ptrSnake->Add(ptrStart2);
+    Sleep(1000);
+    Sleep(1000);
 
-    delete start;
-    delete start2;
+    
+    DrawScreen(ptrSnake);
+    ptrSnake->Move();
+    Sleep(1000);
+    system("CLS");
+    
+    DrawScreen(ptrSnake);
+    ptrSnake->MoveDir(0, -1);
+    ptrSnake->Move();
+    Sleep(1000);
+    system("CLS");
 
+    DrawScreen(ptrSnake);
+    ptrSnake->Move();
+    Sleep(1000);
+    system("CLS");
+
+    DrawScreen(ptrSnake);
+
+
+    delete ptrStart0;
+    delete ptrStart1;
+    delete ptrStart2;
+    delete ptrSnake;
     return 0;
 }
 
